a8b54141936b85186e8067153429df85dc95cd8b
Gabriel Pérez <gabrielprzb@gmail.com>
2020-02-13 21:43:49 -0600

    asd

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,27 @@
 import React from "react";
-import { FaHeart } from "react-icons/fa";
-import { IoMdClose } from "react-icons/io";
+
 
 
 function Card(props) {
+
+
 	return (
 
 		<div className="tinder-top">
-
 
 			<div className="tinder-card">
 
 				<img className="img-tinder" src="https://placeimg.com/600/300/people" />
 
 				<div className="tinder-info">
-					<span className="info-1">Gabriel</span>
+					<span className="info-1">{props.name}</span>
 					<span className="info-2">Passionate</span>
-					<span className="info-3">28 years old</span>
-				</div>
-
-
-
-			</div>
-
-			<div className="actions">
-
-				<div class="icon-container c-close">
-					<IoMdClose />
-				</div>
-				<div class="icon-container c-heart">
-					<FaHeart />
+					<span className="info-3">{props.age} years old</span>
 				</div>
 
 			</div>
+
+			
 
 		</div>
 	);
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,58 +1,115 @@
-import React from "react";
+import React, {useState} from "react";
 import { useQuery } from '@apollo/react-hooks';
+import { useMutation } from "@apollo/react-hooks";
 import gql from 'graphql-tag';
 import Layout from "../common/Layout";
 import Card from "../components/Card";
 
-const GET_EVENTS = gql`
+import { FaHeart } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+
+const GET_USERS = gql`
  {
-    allEvents{
+    getPossibleMatchUsers(id:"5e45fe19d97c7d3518255d79") {
       _id
-      title
-      created_by{
-        first_name
+      name {
+        first
+        last
       }
-      description
+      age
     }
 
   }
 `
 
+const rejection = gql`
+  mutation processUserRejections($id:String!,$userRejected:String!) {
+    processUserRejections(id: $id, userRejected:$userRejected) {
+		_id
+      name {
+        first
+        last
+      }
+      age
+    }
+  }
+`;
+
+const like = gql`
+  mutation processUserLikes($id:String!,$userLiked:String!) {
+    processUserLikes(id: $id, userLiked:$userLiked) {
+		_id
+      name {
+        first
+        last
+      }
+      age
+    }
+  }
+`;
+
 function Home() {
-  /*  const  { loading,data,error  } = useQuery(GET_EVENTS);
- 
-   if(loading) return <h1>Cargando....</h1>
-   if(error) return <p>{error.message}</p> */
-  return (
-    <Layout>
+	const [position, setPosition] = useState(0)
+	const { loading, data, error } = useQuery(GET_USERS);
+	const [execRejection,{data1,loading1,error1}] = useMutation(rejection) 
 
-    <div className="cards-container">
-    <Card name="Andrea"
-        age="20"
-        description="Guapa"
-        id="asd"
-        key="asd"
-      />
-    </div>
+	const [execLike,{data2,loading2,error2}] = useMutation(like) 
 
-    
-      
-
-      {/* 
-              {
-                data.allEvents.map(event => (
-                  <Card title={event.title} 
-                    author={event.created_by.first_name} 
-                    description={event.description}
-                    id={event._id}
-                    key={event._id}
-                  />
-                ))
-              } */}
+	if (loading) return <h1>Cargando....</h1>
+	if (error) return <p>{error.message}</p>
 
 
-    </Layout>
-  );
+
+	const postRejection = () => {
+		
+		let userRejected = data.getPossibleMatchUsers[position]._id;
+		if(position === data.getPossibleMatchUsers.length - 1) return;
+		setPosition( prev => prev + 1 )
+		execRejection({variables:{id: "5e45fe19d97c7d3518255d79", userRejected}})
+
+	}
+
+	const postLike = () => {
+		
+		let userLiked = data.getPossibleMatchUsers[position]._id;
+		if(position === data.getPossibleMatchUsers.length - 1) return;
+		setPosition( prev => prev + 1 )
+		execLike({variables:{id: "5e45fe19d97c7d3518255d79", userLiked}})
+
+	}
+
+
+
+	return (
+		<Layout>
+
+			<div className="cards-container">
+
+
+
+				<Card name={data.getPossibleMatchUsers[position].name.first}
+					age={data.getPossibleMatchUsers[position].age}
+					id={data.getPossibleMatchUsers[position]._id}
+					key={data.getPossibleMatchUsers[position]._id}
+				/>
+
+
+
+				<div className="actions">
+
+					<div class="icon-container c-close" onClick={postRejection}>
+						<IoMdClose />
+					</div>
+					<div class="icon-container c-heart" onClick={postLike}>
+						<FaHeart />
+					</div>
+
+				</div>
+			</div>
+
+
+		</Layout>
+	);
 }
 
 export default Home;
